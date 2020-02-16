64f3f14c5ddfda6b37e6aad4f842f1efeb13235c
Timothy McManus <tmcmanus@firstfloorcreative.com>
2020-02-16 09:46:35 -0500

    Removed text links and added images

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ function getUsers() {
         let output = '';
         data.forEach(function(user){
             output += `
-            <li><a href="${user.html_url}">${user.login}</a></li>
-                    
+            <li><a href="${user.html_url}"><img src="${user.avatar_url}" alt="${user.login}"></a></li>
             `;
         });
         document.getElementById('output').innerHTML = output;
